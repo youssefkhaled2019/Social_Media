75b1a7fcdc85c2a2674da59d8bb3b872ab409db6
youssef <youssefkhaled2019@gmail.com>
2024-02-29 19:05:56 +0200

    day_7 profile_pages

--- blog/views.py
+++ blog/views.py
@@ -26,5 +26,6 @@ def home(request):
     context={"posts":Post.objects.all()}
 
     return render(request,"blog/home.html",context)
+
 def about(request):
     return render(request,"blog/about.html",{"title":"about"})
