f445ed311701d13347a25dc75fa9d0cc6ed9613b
youssef <youssefkhaled2019@gmail.com>
2024-04-14 02:03:59 +0200

    day_8 ProfileUpdateForm

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-
-
 from django.utils import timezone
 from django.contrib.auth.models import User
+from PIL import Image
+
 # Create your models here.
 class Post(models.Model):
     # P_ID=models.
@@ -13,6 +13,14 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
+    
+    def save(self):
+        super().save()
+        img=Image.open(self.image.path)
+        if img.height>300 or img.width>300:
+            size=(300,300)
+            img.thumbnail(size)
+            img.save(self.image.path)
     
     
     
